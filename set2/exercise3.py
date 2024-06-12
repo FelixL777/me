84010cdeb32b32894a89064098c7b72ecc9d7a95
Wendong Luo <Felix.L.7@outlook.com>
2024-06-12 17:36:49 +1000

    update

--- set2/exercise3.py
+++ set2/exercise3.py
@@ -19,7 +19,7 @@ def is_odd(a_number):
     So if a_number modulo two is zero, then it's even.
     """
 
-    return a_number % 2 == 0
+    return a_number % 2 != 0
 
 
 def fix_it(moves=True, should_move=True):
